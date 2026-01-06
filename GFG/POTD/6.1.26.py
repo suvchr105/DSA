79f9d97c813b5d08d3f7000a2706c5871c5ec184
Suvadip Chakraborty <suvadipchakraborty2001@gmail.com>
2026-01-06 09:08:20 +0530

    Day 6: DSA practice

---
+++ GFG/POTD/6.1.26.py
@@ -0,0 +1,43 @@
+# Max Xor Subarray of size K
+# Medium
+
+"""
+Given an array of integers arr[]  and a number k. Return the maximum xor of a subarray of size k.
+
+Note: A subarray is a contiguous part of any given array.
+
+Examples:
+
+Input: arr[] = [2, 5, 8, 1, 1, 3], k = 3
+Output: 15
+Explanation: arr[0] ^ arr[1] ^ arr[2] = 15, which is maximum.
+Input: arr[] = [1, 2, 4, 5, 6] , k = 2
+Output: 6
+Explanation: arr[1] ^ arr[2] = 6, which is maximum.
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 106
+1 ≤ k ≤ arr.size()
+"""
+
+# Code:
+
+class Solution:
+    def maxSubarrayXOR(self, arr, k):
+        # code here
+        n = len(arr)
+        
+        # XOR of first window
+        curr_xor = 0
+        for i in range(k):
+            curr_xor ^= arr[i]
+        
+        max_xor = curr_xor
+        
+        # Sliding window
+        for i in range(k, n):
+            curr_xor ^= arr[i - k]  # remove left element
+            curr_xor ^= arr[i]      # add right element
+            max_xor = max(max_xor, curr_xor)
+        
+        return max_xor
